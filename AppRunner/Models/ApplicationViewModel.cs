2ed8db1fba4d14878abe29d31f0a82f4bfcbc71a
joelhoro <joel@horowitz.com>
2015-04-23 06:54:30 -0400

    Remote inherited method

--- AppRunner/Models/ApplicationViewModel.cs
+++ AppRunner/Models/ApplicationViewModel.cs
@@ -30,19 +30,5 @@ namespace AppRunner.Models
             MessageBox.Show(String.Format("Running {0} with {1}", Executable, CommandLineArgs));
             // run executable somehow
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // This method is called by the Set accessor of each property. 
-        // The CallerMemberName attribute that is applied to the optional propertyName 
-        // parameter causes the property name of the caller to be substituted as an argument. 
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
     }
 }
