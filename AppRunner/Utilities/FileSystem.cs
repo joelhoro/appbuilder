899d209f30b37efe900e7b35a3c061b4b45b1a06
joelhoro <joel@horowitz.com>
2015-05-01 06:57:01 -0400

    Adding FileSystemCache

--- AppRunner/Utilities/FileSystem.cs
+++ AppRunner/Utilities/FileSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,38 @@ namespace AppRunner.Utilities
 {
     public static class FileSystem
     {
+        private static Dictionary<string,List<string>> _solutionscache = new Dictionary<string, List<string>>();
+
+        private static string fileCache = @"C:\temp\filecache";
+        public static void Initialize(bool refreshCache = false)
+        {
+            if (refreshCache)
+            {
+                var root = @"c:\Users\Joel\Documents\Visual Studio 2013\Projects";
+                var files = Directory.GetFiles(root, "*.sln", SearchOption.AllDirectories).ToList();
+                _solutionscache = new Dictionary<string, List<string>>();
+                _solutionscache[root] = files;
+                Serializer.Save(fileCache,_solutionscache);
+            }
+            else
+            {
+                if (!File.Exists(fileCache))
+                {
+                    _solutionscache = new Dictionary<string, List<string>>();
+                    Serializer.Save(fileCache,_solutionscache);                    
+                }
+                _solutionscache = Serializer.Load<Dictionary<string, List<string>>>(fileCache);
+            }
+        }
+
+        public static List<string> FileList(string path)
+        {
+            if (_solutionscache.ContainsKey(path))
+                return _solutionscache[path];
+            else
+                return new List<string>();
+        }
+
         public static string GetFirstDirName(string path, string mask, bool createDirectory = false)
         {
             int counter = 1;
@@ -20,5 +53,6 @@ namespace AppRunner.Utilities
                 Directory.CreateDirectory(dir);
             return dir;
         }
+
     }
 }
